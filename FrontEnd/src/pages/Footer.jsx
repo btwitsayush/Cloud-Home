356d503240a1823f7354e2ef3029788c0c3146f4
Ayush Srivastava <srivastavaayush6393@gmail.com>
2024-07-25 20:06:41 +0530

    Added fotter

---
+++ FrontEnd/src/pages/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CSS/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p className="footer-text">© 2024 Cloud Home. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
