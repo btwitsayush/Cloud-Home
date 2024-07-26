157a22e158b8dd70658d13f017dd8c0c95fc3be1
Ayush Srivastava <srivastavaayush6393@gmail.com>
2024-07-26 17:20:03 +0530

    added a new route for view file

--- BackEnd/router/fileRouter.js
+++ BackEnd/router/fileRouter.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const uploadFileMulter=require('../config/uploadFIleMulter.js')
 
-const{createFile}=require('../controllers/fileControllers.js')
+const{createFile,viewFile}=require('../controllers/fileControllers.js')
 const fileRouter=express.Router();
 
 
 fileRouter.route('/').post(uploadFileMulter.single("file"),createFile);
+fileRouter.route('/:id').post(viewFile);
 
 
 module.exports=fileRouter;
